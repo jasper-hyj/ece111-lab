736727affa7ad1248f4306d84324e935ca2770eb
Jasper Huang <jasper.hyj@gmail.com>
2025-07-28 13:44:05 -0700

    Update johnson counter

--- Homework3/Lab3/johnson_counter/johnson_counter.sv
+++ Homework3/Lab3/johnson_counter/johnson_counter.sv
@@ -4,9 +4,14 @@ module johnson_counter (
   input logic[3:0] load_cnt,
   output logic[3:0] count
 );
- always@(posedge clk or negedge clear) begin
-
-  // Student to add code for Johnson Counter  
-
- end 
+  always @(posedge clk or negedge clear) begin
+    // Student to add code for Johnson Counter
+    if (!clear) begin
+      count = 4'b0000;
+    end else if (!preset) begin
+        count = load_cnt;
+    end else begin
+      count = {~count[0], count[3:1]};
+    end
+  end
 endmodule: johnson_counter
